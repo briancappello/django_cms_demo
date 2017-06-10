15b7031c1fe6c91b22d73c89d27f2d7b91ac26bb
Brian Cappello <briancappello@gmail.com>
2017-06-10 15:43:12 -0400

    update menu so parents of a nested active page show as active themselves

--- app/templatetags/filters.py
+++ app/templatetags/filters.py
@@ -4,6 +4,21 @@ register = template.Library()
 
 
 @register.filter
+def reversed(list):
+    return list[::-1]
+
+
+@register.filter
+def is_active(node):
+    if node.selected:
+        return True
+    for child in node.children:
+        if is_active(child):
+            return True
+    return False
+
+
+@register.filter
 def page_ancestors(page):
     ancestors = []
     while page:
